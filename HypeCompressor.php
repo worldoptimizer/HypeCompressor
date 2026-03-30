92b266be3bbd3045d1aec32e752c873da2299d58
Max Ziebell <1752114+worldoptimizer@users.noreply.github.com>
2026-03-30 11:23:40 +0200

    v1.0.4
    Updated HypeCompressor to improve compression methods and added version history.

--- HypeCompressor.php
+++ HypeCompressor.php
@@ -1,9 +1,24 @@
 <?php
-
-// This helper is based on and extends 
-// https://github.com/worldoptimizer/HypeDocumentLoader-for-PHP
 require_once ("HypeDocumentLoader.php");
- 
+
+/**
+* Hype Compressor for PHP v1.0.4
+* Compress generated script by Tumult Hype 4
+*
+* @author	 Max Ziebell <mail@maxziebell.de>
+*
+*/
+
+/*
+Version history:
+1.0.0 Initial release under existing CJSON license
+1.0.1 Added delete_scene_by_index and delete_scene_by_name
+1.0.2 Refactored and added closure compression
+1.0.3 Set function lookup to identifier, improved string lookup
+1.0.4 Removed rawurlencode from compress(), unicode handling done by JS TextDecoder since HypeCompressor.js 1.0.7
+*/
+
+
 class HypeCompressor extends HypeDocumentLoader{
 	public $loader_object;
 	public $string_lookup = [];
@@ -13,10 +28,21 @@ class HypeCompressor extends HypeDocumentLoader{
 	public $hype_functions;
 	public $hype_functions_minified;
 	public $hype_functions_lookup=[];
+	public $compiled_js;
+
+
+	function __construct($hype_generated_script=null) {
+		parent::__construct($hype_generated_script);
+		if($hype_generated_script && empty($this->loader_object)) $this->loader_object = $this->get_loader_object();
+	}
 
 	public function compress($string) {
-		return base64_encode(gzcompress(rawurlencode($string),9));
+		return base64_encode(gzcompress($string, 9));
 	}
+
+	/**
+	 * helper to compress strings with lookup
+	*/
 
 	private function sort_based_on_count($a, $b) {
 		$aa = $this->string_lookup_count[$a];
@@ -30,55 +56,74 @@ class HypeCompressor extends HypeDocumentLoader{
 	public function compress_strings_in_document_data(){
 		if(empty($this->loader_object)) $this->loader_object = $this->get_loader_object();
 
+		// create lookup counting string occurances
 		$iterator = new RecursiveIteratorIterator(new RecursiveArrayIterator($this->loader_object));
 		foreach($iterator as $key => $value) {
-			if (!is_string($value) || preg_match('/^[0-9"]+$/',$value)) continue;
-			if(strlen($value)>3) $this->string_lookup_count[$value] +=1;
-			if(strlen($key)>5) $this->string_lookup_count[$key] +=1;
+			if (is_string($value) && !preg_match('/^[0-9"]+$/',$value)) {
+				if(strlen($value)>3) $this->string_lookup_count[$value] +=1;	
+			}
+			
+			if (is_string($key) && !preg_match('/^[0-9"]+$/',$key)) {
+				if(strlen($key)>5) $this->string_lookup_count[$key] +=1;
+			}
 		}
 
+		// create string lookup itself based on previous count data
 		foreach($iterator as $key => $value) {
-			if (!is_string($value) || preg_match('/^[0-9"]+$/',$value)) continue;
-			
-			// value string Lookup
-			if(strlen($value)>3 && $this->string_lookup_count[$value] && $this->string_lookup_count[$value]>1){
-				$fid = array_search($value, $this->string_lookup);
-				if($fid===false) $this->string_lookup[] = $value;
+			if (is_string($value) && !preg_match('/^[0-9"]+$/',$value)) {
+				// value string Lookup
+				if(strlen($value)>3 && $this->string_lookup_count[$value] && $this->string_lookup_count[$value]>1){
+					$fid = array_search($value, $this->string_lookup);
+					if($fid===false) $this->string_lookup[] = $value;
+				}
 			}
 
 			// (optional) key string Lookup
-			if(strlen($key)>5 && $this->string_lookup_count[$key] && $this->string_lookup_count[$key]>1){
-				$fid = array_search($key, $this->string_lookup);
-				if($fid===false) $this->string_lookup[] = $key;
+			if (is_string($key) && !preg_match('/^[0-9"]+$/',$key)) {
+				if(strlen($key)>5 && $this->string_lookup_count[$key] && $this->string_lookup_count[$key]>1){
+					$fid = array_search($key, $this->string_lookup);
+					if($fid===false) $this->string_lookup[] = $key;
+				}
 			}
 		}
 
+		// to give smaller ids to strings often used
 		usort($this->string_lookup, array($this, "sort_based_on_count"));
 
+		// apply string lookup to loader object data
 		foreach($iterator as $key => $value) {
-			
-			if (!is_string($value) || preg_match('/^[0-9"]+$/',$value)) continue;
-
-			// value string Lookup assign
-			if(strlen($value)>3 && $this->string_lookup_count[$value] && $this->string_lookup_count[$value]>1){
-				$fid = array_search($value, $this->string_lookup);
-				$iterator->getInnerIterator()->offsetSet($key, '_['.$fid.']');
+			if (is_string($value) && !preg_match('/^[0-9"]+$/',$value)) {
+				// value string Lookup assign
+				if(strlen($value)>3 && $this->string_lookup_count[$value] && $this->string_lookup_count[$value]>1){
+					$fid = array_search($value, $this->string_lookup);
+					$iterator->getInnerIterator()->offsetSet($key, '_['.$fid.']');
+				}
 			}
 
 			// (optional) key string Lookup assign
-			if(strlen($key)>5 && $this->string_lookup_count[$key] && $this->string_lookup_count[$key]>1){
-				$fid = array_search($key, $this->string_lookup);
-				$iterator->getInnerIterator()->offsetUnset($key);
-				$iterator->getInnerIterator()->offsetSet('[_['.$fid.']]', $value);
-			}
+			if (is_string($key) && !preg_match('/^[0-9"]+$/',$key)) {
+				if(strlen($key)>5 && $this->string_lookup_count[$key] && $this->string_lookup_count[$key]>1){
+					$fid = array_search($key, $this->string_lookup);
+					$iterator->getInnerIterator()->offsetUnset($key);
+					$iterator->getInnerIterator()->offsetSet('[_['.$fid.']]', $value);
+				}
+			}	
 		}
 
+		// add lookup to generated script
 		$this->insert_into_document_loader('var _='.$this->encode($this->string_lookup).';');
 	}
 
+
+	/**
+	 * helper to compress scenes objects with lookup (great for symbols)
+	 */
+
 	public function compress_scene_objects(){
 		if(empty($this->loader_object)) $this->loader_object = $this->get_loader_object();
-
+		// dump to loader object to screen and exit this function (debugging)
+		// echo '<pre>'; print_r($this->loader_object); echo '</pre>'; exit;
+		
 		// loop over scenes
 		for ($i = 0; $i < count($this->loader_object->scenes); $i++) {
 			// loop over objects (ids)
@@ -139,19 +184,69 @@ class HypeCompressor extends HypeDocumentLoader{
 
 	}
 
+	/**
+	 * helper to delete scenes 
+	 */
+
+	public function delete_scene_by_index($idx){
+		if(empty($this->loader_object)) $this->loader_object = $this->get_loader_object();
+
+		// determin layouts to delete
+		$layoutsToDelete = $this->loader_object->sceneContainers[$idx]->X;
+
+		// unset layouts
+		foreach ($layoutsToDelete as $j) unset($this->loader_object->scenes[$j]);
+
+		// reindex layouts
+		$this->loader_object->scenes = array_values($this->loader_object->scenes);
+
+		// reduce index of layouts higher by count of deleted
+		for ($i = 0; $i < count($this->loader_object->scenes); $i++) $this->loader_object->scenes[$i]->_ = $i;
+
+		// unset and reindex sceneContainer
+		unset($this->loader_object->sceneContainers[$idx]);
+		$this->loader_object->sceneContainers = array_values($this->loader_object->sceneContainers);
+
+		// walk over them and fix layout indexes
+		for ($i = $idx; $i < count($this->loader_object->sceneContainers); $i++) {
+			for ($j = 0; $j < count($this->loader_object->sceneContainers[$i]->X); $j++) {
+				if($this->loader_object->sceneContainers[$i]->X[$j]>=$layoutsToDelete[0]) 
+					$this->loader_object->sceneContainers[$i]->X[$j] -= count($layoutsToDelete);
+			}
+		}
+	}
+
+	public function delete_scene_by_name($name){
+		if(empty($this->loader_object)) $this->loader_object = $this->get_loader_object();
+
+		// loop over sceneContainer looking for name match
+		for ($i = 0; $i < count($this->loader_object->sceneContainers); $i++) {
+			if ($this->loader_object->sceneContainers[$i]->n == $name){
+				$this->delete_scene_by_index($i);
+			}
+		}
+			
+	}
+
+	/**
+	 * functions to extract JS functions into window scope
+	 */
+
+	public function compress_functions_with_closure(){
+		$this->extract_functions_to_window_scope(true);
+	}
+
 	public function populate_hype_functions ($match) {
-		print_r($match);
-		$this->hype_functions_lookup[stripcslashes($match[1])] = stripcslashes($match[2]);
-		$new_name = 'HYPE_functions[\\"'.$this->document_name.'\\"].'.$match[1];
+		$this->hype_functions_lookup[stripcslashes($match[3])] = stripcslashes($match[2]);
+		$new_name = '_'.$match[3];
 		return 'name:"'.$match[1].'",source:"'.$new_name.'",identifier:"'.$match[3].'"';
 	}
 
 	public function extract_functions_to_window_scope($minify=false){
 		$loader_begin_string = $this->hype_generated_script_parts->loader_begin_string;
+		$this->hype_functions_lookup = [];
 		$this->hype_functions ='';
-		$this->hype_functions .= "\n".'if ("HYPE_functions" in window === false) window.HYPE_functions = {};';
-		$this->hype_functions .= "\n".'HYPE_functions["'.$this->document_name.'"] = {';
-		
+
 		$loader_begin_string = preg_replace_callback(
 			'|name:"(.*?)",source:"(.*?)",identifier:"([0-9]+)"|',
 			array($this, "populate_hype_functions"),
@@ -159,22 +254,38 @@ class HypeCompressor extends HypeDocumentLoader{
 		);
 		
 		foreach($this->hype_functions_lookup as $name => $function){
-			$this->hype_functions .= $name.':'.$function.',';
+			$this->hype_functions .= 'var _'.$name.'='.$function.';';
 		}
 		
-		$this->hype_functions .= "\n".'};';
-
-		if($minify){
-			$this->hype_functions_minified = $this->minify_with_closure_compiler($this->hype_functions);
-			if ($this->hype_functions_minified){
-				$this->insert_into_generated_script($this->hype_functions_minified);
+		if (count($this->hype_functions_lookup)){
+			if($minify){
+				$data = $this->minify_with_closure_compiler($this->hype_functions);
+				if ($data && count($data->errors)){
+					foreach ($data->errors as $error) {
+						$this->insert_into_generated_script("/* \n".$error->error."\n".$error->line."\n*/\n");
+					}
+				}
+				if ($data && !empty($data->compiledCode)) {
+					$this->hype_functions_minified = $data->compiledCode;
+					$this->insert_into_generated_script($this->hype_functions_minified);
+				} else {
+					$this->insert_into_generated_script($this->hype_functions);	
+				}
+			} else {
+				$this->insert_into_generated_script($this->hype_functions);
 			}
-		} else {
-			$this->insert_into_generated_script($this->hype_functions);
-		}
 
-		$this->hype_generated_script_parts->loader_begin_string = $loader_begin_string;
+			$this->hype_generated_script_parts->loader_begin_string = $loader_begin_string;
+			return true;
+		} else {
+			return false;
+		}
 	}
+
+
+	/**
+	 * helper to format string for RFC2045
+	 */
 
 	public function RFC2045($string, $append='', $prepend=''){
 		$first_chunk = 500-(strlen($append)+2);
@@ -194,6 +305,10 @@ class HypeCompressor extends HypeDocumentLoader{
 		return $string_RFC2045;
 	}
 
+	/**
+	 * functions for true ZIP compression using 
+	 * https://cdn.jsdelivr.net/gh/worldoptimizer/HypeCompressor/HypeCompressor.min.js
+	 */
 
 	public function compress_and_wrap_with_run($js){
 		$js_compressed = $this->compress($js);
@@ -204,20 +319,19 @@ class HypeCompressor extends HypeDocumentLoader{
 		return file_get_contents('https://cdn.jsdelivr.net/gh/worldoptimizer/HypeCompressor/HypeCompressor.min.js')."\n";
 	}
 
+	/**
+	 * Helper to communicate with closure API
+	 */
+
 	public function minify_with_closure_compiler($js) {
+		if ($this->compiled_js) return $this->compiled_js;
 		if (empty($js)) return;
 
-		// preapre arguments
-		$apiArgs = array(
-			'compilation_level' => 'SIMPLE_OPTIMIZATIONS',
-			'output_format' => 'text',
-			'output_info' => 'compiled_code'
-		);
-
 		$args = 'js_code=' . urlencode($js);
-		foreach ($apiArgs as $key => $value) {
-			$args .= '&' . $key . '=' . urlencode($value);
-		}
+		$args .= '&compilation_level=SIMPLE_OPTIMIZATIONS';
+		$args .= '&output_format=json';
+		$args .= '&output_info=compiled_code';
+		$args .= '&output_info=errors';
 
 		//API call using cURL
 		$call = curl_init();
@@ -233,7 +347,8 @@ class HypeCompressor extends HypeDocumentLoader{
 		curl_close($call);
 
 		// return minified
-		return ($jscomp);
+		$this->compiled_js = json_decode($jscomp);
+		return $this->compiled_js;
 	}
 	
 }
